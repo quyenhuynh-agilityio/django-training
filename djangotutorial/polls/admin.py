5f767167f400bfec7e76a750d80ec3cacc00f92f
Quyen Huynh <quyen.huynh@asnet.com.vn>
2025-04-03 15:14:24 +0700

    feat: update gitignore file

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -17,6 +17,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
+    list_per_page = 1
 
 
 class CustomAdminSite(admin.AdminSite):
